7273eadabb665929b39d167d006ec6d3ddd06443
Michel Oliveira e Oliveira <oliveira_michel@outlook.com>
2020-04-28 17:32:13 -0300

    artigo github

--- menu.js
+++ menu.js
@@ -5,11 +5,23 @@ function filterHashTag(hashtag){
 
         var inputs = gridItems[g].getElementsByTagName("input");
         for (var i = 0; i < inputs.length; i++) {
-            if(inputs[i].name == "hashtags")
-                if(inputs[i].value == hashtag)
-                    gridItems[g].style.display = "block";
-                else
-                    gridItems[g].style.display = "none";
+            if(inputs[i].name == "hashtags") {
+                if (inputs[i].value != "")
+                {
+                    var hashtags = inputs[i].value.split(" ");
+                    for (var h = 0; h < hashtags.length; h++) {
+                        if(hashtags[h] == hashtag){
+                            gridItems[g].style.display = "block";
+                            break;
+                        }
+                        else{
+                            gridItems[g].style.display = "none";
+                        }
+                    }
+                }
+            }
         }
     }
+
+    resizeAllGridItems();
 }
